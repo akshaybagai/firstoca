99dbc4eb1d84e1d58ed356ed04a4aabccd5c474b
Akshay Bagai <Akshay.Bagai@ey.com>
2020-04-28 10:15:55 -0400

    Initial commit

---
+++ src/main/java/com/oca/hello/HelloController.java
@@ -0,0 +1,16 @@
+package com.oca.hello;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+
+}
